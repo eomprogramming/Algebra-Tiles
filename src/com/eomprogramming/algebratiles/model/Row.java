530db1dba68c2f4ebf65d7c87aa95bbcd2b996e3
Hao-Wei <hao5838@hotmail.com>
2013-04-14 18:41:57 -0400

    FINAL COMMIT (hopefully)

--- src/com/eomprogramming/algebratiles/model/Row.java
+++ src/com/eomprogramming/algebratiles/model/Row.java
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-package com.eomprogramming.algebratiles.model;
-
-
-import java.util.ArrayList;
-
-public class Row {
-	
-	private ArrayList<Tile> tiles;
-	private int position;
-	
-	public Row(int pos){
-		tiles = new ArrayList<Tile>();
-		setPosition(pos);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public Row(int pos, ArrayList<Tile> tiles){
-		this.tiles = (ArrayList<Tile>) tiles.clone();
-		setPosition(pos);
-	}
-	
-	public void add(Tile t){
-		if(t != null)
-			tiles.add(t);
-	}
-
-	public int getPosition() {
-		return position;
-	}
-
-	public void setPosition(int p) {
-		position = p;
-	}	
-	
-	public ArrayList<Tile> getTiles(){
-		return tiles;
-	}
-	
-	public Tile get(int i)
-	{
-		return tiles.get(i);
-	}
-	
-	@Override
-	public Row clone()
-	{
-		Row r = new Row(position);
-		ArrayList<Tile> t = new ArrayList<Tile>();
-		for(Tile a: tiles)
-			t.add(a.clone());
-		r.tiles = t;
-		return r;
-	}
-}
-=======
 package com.eomprogramming.algebratiles.model;
 
 
@@ -107,4 +51,4 @@ public class Row {
 		return r;
 	}
 }
->>>>>>> c54d3748c8a02a6e31159b198959b4c4c1bf8f80
+
